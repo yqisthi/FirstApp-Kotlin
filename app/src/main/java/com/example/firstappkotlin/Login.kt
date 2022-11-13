7668af7d11ff10cd8f2a0ae51382d444cc841923
Yusuf Qisthi <yqisthi2002@gmail.com>
2022-11-13 21:59:00 +0700

    firebase

--- app/src/main/java/com/example/firstappkotlin/Login.kt
+++ app/src/main/java/com/example/firstappkotlin/Login.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -12,12 +14,13 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
-
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
         btnLogin = findViewById(R.id.btnLogin)
@@ -27,5 +30,23 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+        btnLogin.setOnClickListener() {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+            login(email, password)
+        }
+
+    }
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@Login, "Invalid email or password", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
